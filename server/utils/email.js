0a33e5c76a7a7ab390adda890363cccda14cc305
buchyomt <riskythenextclan@gmail.com>
2026-06-15 11:37:58 +0100

    fix: remove aggressive timeouts from email config

--- server/utils/email.js
+++ server/utils/email.js
@@ -8,9 +8,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000, // 10 seconds max
-  greetingTimeout: 5000,
-  socketTimeout: 15000,
   logger: true, 
   debug: true,  
   tls: {
